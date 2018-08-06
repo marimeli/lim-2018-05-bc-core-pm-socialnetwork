fbd0cb06381efecf5f264fa8dc763551d390aca2
marimeli <marimel.casas@gmail.com>
2018-08-06 03:02:25 -0500

    arreglando modales a modo responsive

--- src/js/main.js
+++ src/js/main.js
@@ -43,7 +43,7 @@ const postComposerContainer = document.getElementById('post-composer-container')
 const statusOfPrivacy = document.getElementById('status-of-privacy');
 
 const feedButton = document.getElementById('feed-button');
-const profileButton = document.getElementById('profileButton');
+const profileButton = document.getElementById('profile-button');
 
 // FUNCIONES MODALES
 //Login
@@ -103,3 +103,27 @@ sendPostButton.addEventListener('click', writtingPost);
 
 feedButton.addEventListener('click', showFeed);
 profileButton.addEventListener('click', hideContainers);
+
+
+// Accordion
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-theme-d1";
+    } else {
+        x.className = x.className.replace("w3-show", "");
+        x.previousElementSibling.className =
+            x.previousElementSibling.className.replace(" w3-theme-d1", "");
+    }
+};
+
+// Used to toggle the menu on smaller screens when clicking on the menu button
+function openNav() {
+    var logged = document.getElementById("logged-in");
+    if (logged.className.indexOf("w3-show") == -1) {
+      logged.className += " w3-show";
+    } else {
+      logged.className = logged.className.replace(" w3-show", "");
+    }
+};
