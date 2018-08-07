5dd9c514942858e2843128360ea887530355c9a8
marimeli <marimel.casas@gmail.com>
2018-08-06 19:02:41 -0500

    separando funcionalidades, falta arreglar datajs

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,4 @@
-/// DOM
-//Llamando modal
-const callModalRegister = document.getElementById('call-modal-register');
-const callModalLogin = document.getElementById('call-modal-login');
-//Login modal botones
-const cancelButton = document.getElementById('cancel-button');
-const xButton = document.getElementById('x-button');
-
-const mail = document.getElementById('email');
-const errorEmail = document.getElementById('error-email')
-const password = document.getElementById('password');
-const errorPassword = document.getElementById('error-password');
-const loginButton = document.getElementById('login-btn');
-const facebookButton = document.getElementById('facebook-button');
-const googleButton = document.getElementById('google-button');
-
-//Register modal botones
-const cancelRegButton = document.getElementById('register-cancel-button');
-const xRegButton = document.getElementById('x-register-button');
-
-const emailRegister = document.getElementById('email-register');
-const adviceEmailRegister = document.getElementById('advice-emailRegister');
-const passwordRegister = document.getElementById('password-register');
-const advicePasswordRegister = document.getElementById('advice-passwordRegister');
-
-const registerButton = document.getElementById('register-button');
-
-/* MURO */
+/* Almacenando las variables para la selección de DOM */
 
 //Logout
 const logoutButton = document.getElementById('logout');
@@ -45,86 +18,40 @@ const statusOfPrivacy = document.getElementById('status-of-privacy');
 const feedButton = document.getElementById('feed-button');
 const profileButton = document.getElementById('profile-button');
 
-// FUNCIONES MODALES
-//Login
-const showModal = () => {
-  document.getElementById('loginModal').style.display = 'block';
-}
 
-const dontShowModal = () => {
-  document.getElementById('loginModal').style.display = 'none';
-}
+/* FUNCIONES MURO */
 
-const xButtonClose = () => {
-  document.getElementById('loginModal').style.display = 'none';
-}
+// Accordion
+const myFunction = (id) => {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+      x.previousElementSibling.className += " w3-theme-d1";
+  } else {
+      x.className = x.className.replace("w3-show", "");
+      x.previousElementSibling.className =
+          x.previousElementSibling.className.replace(" w3-theme-d1", "");
+  }
+};
 
-//Registro
-const showModalRegister = () => {
-  document.getElementById('registerModal').style.display = 'block';
-}
-const dontShowModalRegister = () => {
-  document.getElementById('registerModal').style.display = 'none';
-}
-const xButtonCloseRegister = () => {
-  document.getElementById('registerModal').style.display = 'none';
-}
+// Used to toggle the menu on smaller screens when clicking on the menu button
+const openNav = () => {
+  var logged = document.getElementById("logged-in");
+  if (logged.className.indexOf("w3-show") == -1) {
+    logged.className += " w3-show";
+  } else {
+    logged.className = logged.className.replace(" w3-show", "");
+  }
+};
 
-// LLAMADOS
-// Modal login
-callModalLogin.addEventListener('click', showModal);
-cancelButton.addEventListener('click', dontShowModal);
-xButton.addEventListener('click', xButtonClose);
-//Modal register 
-callModalRegister.addEventListener('click', showModalRegister);
-cancelRegButton.addEventListener('click', dontShowModalRegister);
-xRegButton.addEventListener('click', xButtonCloseRegister);
+/* EVENTOS DEL DOM */
 
-//Registro
-registerButton.addEventListener('click', registerWithFirebase);
-
-//Login con Email
-loginButton.addEventListener('click', loginWithFirebase);
-
-//Login con Facebook
-facebookButton.addEventListener('click', facebookLoginWithFirebase);
-
-//Login con Google
-googleButton.addEventListener('click', googleLoginWithFirebase);
-
-//Logout
 logoutButton.addEventListener('click', logoutWithFirebase);
 
-/* MURO */
-
 sendPostButton.addEventListener('click', writtingPost);
-
-//
 
 feedButton.addEventListener('click', showFeed);
 
 profileButton.addEventListener('click', hideContainers);
 
 
-// Accordion
-function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += " w3-theme-d1";
-    } else {
-        x.className = x.className.replace("w3-show", "");
-        x.previousElementSibling.className =
-            x.previousElementSibling.className.replace(" w3-theme-d1", "");
-    }
-};
-
-// Used to toggle the menu on smaller screens when clicking on the menu button
-function openNav() {
-    var logged = document.getElementById("logged-in");
-    if (logged.className.indexOf("w3-show") == -1) {
-      logged.className += " w3-show";
-    } else {
-      logged.className = logged.className.replace(" w3-show", "");
-    }
-};
